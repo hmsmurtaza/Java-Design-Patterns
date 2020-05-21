4054d7415eb473845698eef3c2441edb1318da7c
MSMurtaza <shoaibmurtaza1@yahoo.com>
2020-05-22 00:00:45 +0500

    added xpay and xpayimplementation files

--- AdapterPattern/src/Xpay.java
+++ AdapterPattern/src/Xpay.java
@@ -1,2 +1,17 @@
 public interface Xpay {
+    public String getCreditCardNumber();
+    public String getCustomerName();
+    public String getCardExpiryMonth();
+
+    public String getCardExpiryYear();
+
+    public Short getCardCVVNumber();
+    public Double getAmount();
+
+    public void setCreditCardNumber(String creditCardNumber);
+    public void setCustomerName(String customerName);
+    public void setCardExpiryMonth(String cardExpiryMonth);
+    public void setCardExpiryYear(String cardExpiryYear);
+    public void setCardCVVNumber(short cardCVVNumber);
+    public void setAmount(Double amount);
 }
--- AdapterPattern/src/XpayImplementation.java
+++ AdapterPattern/src/XpayImplementation.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
+public class XpayImplementation implements Xpay {
+    private String creditCardNumber;
+    private String customerName;
+    private String cardExpiryMonth;
+    private String cardExpiryYear;
+    private short cardCVVNumber;
+    private Double amount;
 
-public class XpayImplementation {
+    @Override
+    public String getCreditCardNumber() {
+        return null;
+    }
+
+    @Override
+    public String getCustomerName() {
+        return null;
+    }
+
+    @Override
+    public String getCardExpiryMonth() {
+        return null;
+    }
+
+    @Override
+    public String getCardExpiryYear() {
+        return null;
+    }
+
+    @Override
+    public Short getCardCVVNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getAmount() {
+        return null;
+    }
+
+    @Override
+    public void setCreditCardNumber(String creditCardNumber) {
+
+    }
+
+    @Override
+    public void setCustomerName(String customerName) {
+
+    }
+
+    @Override
+    public void setCardExpiryMonth(String cardExpiryMonth) {
+
+    }
+
+    @Override
+    public void setCardExpiryYear(String cardExpiryYear) {
+
+    }
+
+    @Override
+    public void setCardCVVNumber(short cardCVVNumber) {
+
+    }
+
+    @Override
+    public void setAmount(Double amount) {
+
+    }
 }
